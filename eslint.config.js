b75d28e17f60dda10ba451db5ffe7d45e55ed891
김진욱 <jwook0318@gmail.com>
2024-03-04 16:36:04 +0900

    feat: eslint config setting

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+  },
+  {
+    // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
+    files: ['**/*.vue'],
+    rules: {
+      'vue/operator-linebreak': ['error', 'before'],
+    },
+  },
+  {
+    // Without `files`, they are general rules for all files
+    rules: {
+      'style/semi': ['error', 'never'],
+    },
+  },
+)

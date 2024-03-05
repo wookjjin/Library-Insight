28ea4422cd200e72821dad6139f0cf1dc427dfa1
채정훈 <wjdgns4035@iabacus.co.kr>
2024-03-05 17:00:03 +0900

    codemirror-component-example

---
+++ pages/examples/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CodeMirror from '../components/CodeMirror.vue'
+import { codeValue } from '../assets/codeVault'
+
+const code = ref<string>(`${codeValue}`)
+</script>
+
+<template>
+  <div>
+    <CodeMirror :code="code" />
+  </div>
+</template>
+
+<style scoped></style>
